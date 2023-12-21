4910e65d39879b7244c3b61a7baa32bb5f12ab6f
Vu Trung Nam <vutrungnam2002@gmail.com>
2023-12-21 17:42:47 +0700

    update

---
+++ src/redux/exportModule.ts
@@ -0,0 +1,127 @@
+import {
+  useGetBillsQuery,
+  useGetBillQuery,
+  useAddBillMutation,
+  useUpdateBillMutation,
+  useDeleteBillMutation
+} from '@/redux/createApi/bill'
+
+import {
+  useGetCustomersQuery,
+  useGetCustomerQuery,
+  useAddCustomerMutation,
+  useUpdateCustomerMutation,
+  useDeleteCustomerMutation
+} from '@/redux/createApi/customer'
+
+import {
+  useGetRegulationsQuery,
+  useGetRegulationQuery,
+  useAddRegulationMutation,
+  useUpdateRegulationMutation,
+  useDeleteRegulationMutation
+} from '@/redux/createApi/regulation'
+
+import {
+  useGetReportsQuery,
+  useGetReportQuery,
+  useAddReportMutation,
+  useUpdateReportMutation,
+  useDeleteReportMutation
+} from '@/redux/createApi/report'
+import {
+  useGetRoomRentalsQuery,
+  useGetRoomRentalQuery,
+  useAddRoomRentalMutation,
+  useUpdateRoomRentalMutation,
+  useDeleteRoomRentalMutation
+} from '@/redux/createApi/room-rental'
+import {
+  useGettypeRoomsQuery,
+  useGettypeRoomQuery,
+  useAddtypeRoomMutation,
+  useUpdatetypeRoomMutation,
+  useDeletetypeRoomMutation
+} from '@/redux/createApi/room-type'
+import {
+  useGetRoomsQuery,
+  useGetRoomQuery,
+  useAddRoomMutation,
+  useUpdateRoomMutation,
+  useDeleteRoomMutation
+} from '@/redux/createApi/room'
+import {
+  useGetServiceDetailsQuery,
+  useGetServiceDetailQuery,
+  useAddServiceDetailMutation,
+  useUpdateServiceDetailMutation,
+  useDeleteServiceDetailMutation
+} from '@/redux/createApi/service-detail'
+import {
+  useGetServiceTypesQuery,
+  useGetServiceTypeQuery,
+  useAddServiceTypeMutation,
+  useUpdateServiceTypeMutation,
+  useDeleteServiceTypeMutation
+} from '@/redux/createApi/service-type'
+import {
+  useGetServicesQuery,
+  useGetServiceQuery,
+  useAddServiceMutation,
+  useUpdateServiceMutation,
+  useDeleteServiceMutation
+} from '@/redux/createApi/service'
+
+// export api
+export {
+  useGetBillsQuery,
+  useGetBillQuery,
+  useAddBillMutation,
+  useUpdateBillMutation,
+  useDeleteBillMutation,
+  useGetCustomersQuery,
+  useGetCustomerQuery,
+  useAddCustomerMutation,
+  useUpdateCustomerMutation,
+  useDeleteCustomerMutation,
+  useGetRegulationsQuery,
+  useGetRegulationQuery,
+  useAddRegulationMutation,
+  useUpdateRegulationMutation,
+  useDeleteRegulationMutation,
+  useGetReportsQuery,
+  useGetReportQuery,
+  useAddReportMutation,
+  useUpdateReportMutation,
+  useDeleteReportMutation,
+  useGetRoomRentalsQuery,
+  useGetRoomRentalQuery,
+  useAddRoomRentalMutation,
+  useUpdateRoomRentalMutation,
+  useDeleteRoomRentalMutation,
+  useGettypeRoomsQuery,
+  useGettypeRoomQuery,
+  useAddtypeRoomMutation,
+  useUpdatetypeRoomMutation,
+  useDeletetypeRoomMutation,
+  useGetRoomsQuery,
+  useGetRoomQuery,
+  useAddRoomMutation,
+  useUpdateRoomMutation,
+  useDeleteRoomMutation,
+  useGetServiceDetailsQuery,
+  useGetServiceDetailQuery,
+  useAddServiceDetailMutation,
+  useUpdateServiceDetailMutation,
+  useDeleteServiceDetailMutation,
+  useGetServiceTypesQuery,
+  useGetServiceTypeQuery,
+  useAddServiceTypeMutation,
+  useUpdateServiceTypeMutation,
+  useDeleteServiceTypeMutation,
+  useGetServicesQuery,
+  useGetServiceQuery,
+  useAddServiceMutation,
+  useUpdateServiceMutation,
+  useDeleteServiceMutation
+}
